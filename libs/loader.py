0840ffa295fe60d35bff765b5ff98bc607f84507
ms903-github <ms903.takahashi@gmail.com>
2020-04-09 10:55:25 +0000

    add main.py

--- libs/loader.py
+++ libs/loader.py
@@ -9,19 +9,28 @@ import pandas as pd
 from PIL import Image
 import random
 
+# if data is given as (path label \n) in txt file
 def load_pict(load_path, transform=None):
     class MyDataset(Dataset):
         def __init__(self, file_path, transform):
-            self.df = pd.read_csv(file_path)
+            pathlist = []
+            labellist = []
+            with open(file_path, "r") as f:
+                lines = f.readlines()
+            for line in lines:
+                path, label = line.split(" ")
+                pathlist.append(path)
+                labellist.append(int(label))
+            self.pathlist = pathlist
+            self.labellist = labellist
             self.transform = transform
 
         def __len__(self):
             return len(self.df)
         
         def __getitem__(self, idx):
-            img_path = self.df.iloc[idx]["image_path"]
-            img_path = change_path(img_path)
-            label = self.df.iloc[idx]["label"]
+            img_path = self.pathlist[idx]
+            label = self.labellist[idx]
             img = Image.open(img_path)
             if self.transform:
                 img = self.transform(img)
---
+++ main.py
@@ -0,0 +1,354 @@
+import argparse
+import os
+import pandas as pd
+import sys
+import shutil
+import time
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import yaml
+
+from addict import Dict
+from sklearn.metrics import f1_score
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, ToTensor, RandomResizedCrop
+from torchvision.transforms import ColorJitter, RandomHorizontalFlip, Normalize
+
+from libs.functions import AverageMeter, ProgressMeter, accuracy
+from libs.loader import load_pict
+# from libs.models import Classifier
+# from libs.transformer import MyTransformer
+
+
+def get_arguments():
+    '''
+    parse all the arguments from command line inteface
+    return a list of parsed arguments
+    '''
+
+    parser = argparse.ArgumentParser(
+        description='train a network for image classification with Flowers Recognition Dataset')
+    parser.add_argument('config', type=str, help='path of a config file')
+    parser.add_argument(
+        '--resume',
+        action='store_true',
+        help='Add --resume option if you start training from checkpoint.'
+    )
+
+    return parser.parse_args()
+
+
+def train(train_loader, model, criterion, optimizer, epoch, device):
+    # 平均を計算してくれるクラス
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+
+    # 進捗状況を表示してくれるクラス
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses, top1],
+        prefix="Epoch: [{}]".format(epoch)
+    )
+
+    # keep predicted results and gts for calculate F1 Score
+    gts = []
+    preds = []
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, sample in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        x = sample['img']
+        t = sample['class_id']
+
+        x = x.to(device)
+        t = t.to(device)
+
+        batch_size = x.shape[0]
+
+        # compute output and loss
+        output = model(x)
+        loss = criterion(output, t)
+
+        # measure accuracy and record loss
+        acc1 = accuracy(output, t, topk=(1,))
+        losses.update(loss.item(), batch_size)
+        top1.update(acc1[0].item(), batch_size)
+
+        # keep predicted results and gts for calculate F1 Score
+        _, pred = output.max(dim=1)
+        gts += list(t.to("cpu").numpy())
+        preds += list(pred.to("cpu").numpy())
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # show progress bar per 50 iteration
+        if i != 0 and i % 50 == 0:
+            progress.display(i)
+
+    # calculate F1 Score
+    f1s = f1_score(gts, preds, average="macro")
+
+    return losses.avg, top1.avg, f1s
+
+
+def validate(val_loader, model, criterion, device):
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+
+    # keep predicted results and gts for calculate F1 Score
+    gts = []
+    preds = []
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        for i, sample in enumerate(val_loader):
+            x = sample['img']
+            t = sample['class_id']
+            x = x.to(device)
+            t = t.to(device)
+
+            batch_size = x.shape[0]
+
+            # compute output and loss
+            output = model(x)
+            loss = criterion(output, t)
+
+            # measure accuracy and record loss
+            acc1 = accuracy(output, t, topk=(1,))
+            losses.update(loss.item(), batch_size)
+            top1.update(acc1[0].item(), batch_size)
+
+            # keep predicted results and gts for calculate F1 Score
+            _, pred = output.max(dim=1)
+            gts += list(t.to("cpu").numpy())
+            preds += list(pred.to("cpu").numpy())
+
+    f1s = f1_score(gts, preds, average="macro")
+
+    return losses.avg, top1.avg, f1s
+
+
+def main():
+    args = get_arguments()
+
+    # configuration
+    CONFIG = Dict(yaml.safe_load(open(args.config)))
+
+    if not os.path.exists(CONFIG.result_path):
+        os.makedirs(CONFIG.result_path)
+    shutil.copy(args.config, result_path)
+
+    # cpu or cuda
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if device == 'cuda':
+        torch.backends.cudnn.benchmark = True
+    else:
+        print('You have to use GPUs because training CNN is computationally expensive.')
+        sys.exit(1)
+
+    transform = transforms.Compose(
+            [transforms.RandomHorizontalFlip(),
+             transforms.Resize((args.resolution,args.resolution)),
+             transforms.RandomResizedCrop(size=(args.resolution,args.resolution)),
+             transforms.ToTensor()
+             ])
+
+    # Dataloader
+    # if data are given in txt file format
+    train_data = load_pict(CONFIG.path_data, transform=transforms)
+
+    # if data are given in directory format
+    train_data = load_pict2(CONFIG.data_path, transform=transforms)
+
+    train_loader = DataLoader(
+        train_data,
+        batch_size=CONFIG.batch_size,
+        shuffle=True,
+        num_workers=CONFIG.num_workers,
+        pin_memory=True,
+        drop_last=True
+    )
+
+    val_loader = DataLoader(
+        val_data,
+        batch_size=1,
+        shuffle=False,
+        num_workers=CONFIG.num_workers,
+        pin_memory=True
+    )
+
+    # load model
+    print('\n------------------------Loading Model------------------------\n')
+
+    # the number of classes
+    n_classes = CONFIG.n_classes
+
+    if CONFIG.model == 'resnet18':
+        print('ResNet18 will be used as a model.')
+        model = torchvision.models.resnet18(pretrained=True)
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(
+            in_features=in_features,
+            out_features=n_classes,
+            bias=True
+        )
+    elif CONFIG.model == 'resnet34':
+        print('ResNet34 will be used as a model.')
+        model = torchvision.models.resnet34(pretrained=True)
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(
+            in_features=in_features,
+            out_features=n_classes,
+            bias=True
+        )
+    else:
+        print('There is no model appropriate to your choice. '
+              'You have to choose resnet18 or resnet34 as a model in config.yaml')
+        sys.exit(1)
+
+    # send the model to cuda/cpu
+    model.to(device)
+
+    if CONFIG.optimizer == 'Adam':
+        print(CONFIG.optimizer + ' will be used as an optimizer.')
+        optimizer = optim.Adam(model.parameters(), lr=CONFIG.learning_rate)
+    elif CONFIG.optimizer == 'SGD':
+        print(CONFIG.optimizer + ' will be used as an optimizer.')
+        optimizer = optim.SGD(
+            model.parameters(),
+            lr=CONFIG.learning_rate,
+            momentum=CONFIG.momentum,
+            dampening=CONFIG.dampening,
+            weight_decay=CONFIG.weight_decay,
+            nesterov=CONFIG.nesterov
+        )
+    else:
+        print(
+            'There is no optimizer which suits to your option.'
+            'You have to choose SGD or Adam as an optimizer in config.yaml')
+
+    # learning rate scheduler
+    if CONFIG.scheduler == 'onplateau':
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, 'min', patience=CONFIG.lr_patience
+        )
+    else:
+        scheduler = None
+
+    begin_epoch = 0
+
+    # create log
+    best_acc1 = 0
+    log = pd.DataFrame(
+        columns=[
+            'epoch', 'lr', 'train_loss', 'val_loss',
+            'train_acc@1', 'val_acc@1', 'train_f1s', 'val_f1s'
+        ]
+    )
+
+    # criterion for loss
+    if CONFIG.class_weight:
+        criterion = nn.CrossEntropyLoss(
+            weight=get_class_weight(n_classes=n_classes).to(device)
+        )
+    else:
+        criterion = nn.CrossEntropyLoss()
+
+    # train and validate model
+    print('\n------------------------Start training------------------------\n')
+    train_losses = []
+    val_losses = []
+    train_top1_accuracy = []
+    val_top1_accuracy = []
+    train_f1_score = []
+    val_f1_score = []
+
+    for epoch in range(begin_epoch, CONFIG.max_epoch):
+
+        # training
+        train_loss, train_acc1, train_f1s = train(
+            train_loader, model, criterion, optimizer, epoch, device)
+
+        train_losses.append(train_loss)
+        train_top1_accuracy.append(train_acc1)
+        train_f1_score.append(train_f1s)
+
+        # validation
+        val_loss, val_acc1, val_f1s = validate(
+            val_loader, model, criterion, device)
+
+        val_losses.append(val_loss)
+        val_top1_accuracy.append(val_acc1)
+        val_f1_score.append(val_f1s)
+
+        # scheduler
+        if scheduler is not None:
+            scheduler.step(val_loss)
+
+        # save a model if top1 acc is higher than ever
+        if best_acc1 < val_top1_accuracy[-1]:
+            best_acc1 = val_top1_accuracy[-1]
+            torch.save(
+                model.state_dict(),
+                os.path.join(CONFIG.result_path, 'best_acc1_model.prm')
+            )
+
+        # save checkpoint every epoch
+        save_checkpoint(
+            CONFIG.result_path, epoch, model, optimizer, best_acc1, scheduler)
+
+        # save a model every 10 epoch
+        if epoch % 10 == 0 and epoch != 0:
+            save_checkpoint(
+                CONFIG.result_path, epoch, model, optimizer,
+                best_acc1, scheduler, add_epoch2name=True
+            )
+
+        
+        # write logs to dataframe and csv file
+        tmp = pd.Series([
+            epoch,
+            optimizer.param_groups[0]['lr'],
+            train_losses[-1],
+            val_losses[-1],
+            train_top1_accuracy[-1],
+            val_top1_accuracy[-1],
+            train_f1_score[-1],
+            val_f1_score[-1]
+        ], index=log.columns
+        )
+
+        log = log.append(tmp, ignore_index=True)
+        log.to_csv(os.path.join(CONFIG.result_path, 'log.csv'), index=False)
+
+        print(
+            'epoch: {}\tlr: {}\ttrain loss: {:.4f}\tval loss: {:.4f}\tval_acc1: {:.5f}\tval_f1s: {:.5f}'
+            .format(epoch, optimizer.param_groups[0]['lr'], train_losses[-1],
+                    val_losses[-1], val_top1_accuracy[-1], val_f1_score[-1])
+        )
+
+    # save models
+    torch.save(
+        model.state_dict(), os.path.join(CONFIG.result_path, 'final_model.prm'))
+
+
+if __name__ == '__main__':
+    main()
